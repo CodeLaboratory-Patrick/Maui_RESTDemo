dfedb5ea8e1a74cf9fc9a07ade162bb9d48eab09
Donghwi_Park <gptnc5100@gmail.com>
2024-10-24 09:36:04 +0200

    Creating the HTTP Client object.

---
+++ RESTDemo/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RESTDemo
+{
+    public class MainViewModel
+    {
+        HttpClient client;
+        JsonSerializerOptions _serializerOptions;
+
+        public MainViewModel()
+        {
+            client = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+        }
+        public ICommand AddUserCommand =>
+            new Command(() =>
+            {
+
+            });
+
+    }
+}
